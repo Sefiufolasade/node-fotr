b583d969ac9fcfa6eaa77a80d763378c380787ad
Sefiufolasade <107896813+Sefiufolasade@users.noreply.github.com>
2026-06-24 06:50:22 +0100

    Update index.js

--- node-fotr/index.js
+++ node-fotr/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const fs = require("fs");
+//const fs = require("fs");
 require("dotenv").config();
 const app = express();
 
